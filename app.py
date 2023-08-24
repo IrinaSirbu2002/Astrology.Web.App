1ea88bc9d90a4d3ddbbcb26ed30273a740c87b7e
Sirbu Irina <irinasirbu0326@gmail.com>
2023-08-24 20:06:01 +0300

    another change

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ import csv
 app = Flask(__name__)
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
 
 # @app.after_request
 # def after_request(response):
